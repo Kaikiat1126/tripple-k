f3a01824c749d8e0e1eb3292c42c0ff9dc6561ff
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2024-05-18 17:21:31 +0800

    feat: hypothesis_7 done

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,11 +6,19 @@ app = FastAPI()
 dataset_filepath = 'dataset/Fraudulent_E-Commerce_Transaction_Data.csv'
 dataset = pd.read_csv(dataset_filepath)
 
+hypo7 = None
 hypo8 = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello Triple K"}
+
+@app.get("/hypothesis_7")
+async def hypothesis_7():
+    global hypo7
+    if hypo7 is None:
+        hypo7 = analyse_device_used(dataset)
+    return hypo7
 
 
 @app.get("/hypothesis_8")
@@ -19,6 +27,25 @@ async def hypothesis_8():
     if hypo8 is None:
         hypo8 = analyse_most_scam_age_group(dataset)
     return hypo8
+
+def analyse_device_used(dataset):
+    dataset['Is Fraudulent'] = dataset['Is Fraudulent'].apply(lambda x: True if x == 1 else False)
+    result = []
+    fraud_counts = dataset.groupby('Device Used')['Is Fraudulent'].sum().reset_index()
+    total_counts = dataset['Device Used'].value_counts().reset_index()
+    total_counts.columns = ['Device Used', 'Total Transactions']
+
+    for i in range(len(fraud_counts)):
+        temp_device = fraud_counts.iloc[i]['Device Used']
+        temp_count: int = fraud_counts.iloc[i]['Is Fraudulent']
+        temp_total: int = total_counts[total_counts['Device Used'] == temp_device]['Total Transactions'].values[0]
+        temp = {"device_used": temp_device, "fraud_rate": temp_count / temp_total, "fraud_count": int(str(temp_count)), "total_transaction": int(str(temp_total))}
+        result.append(temp)
+
+    global hypo7
+    hypo7 = result
+
+    return result
 
 
 def analyse_most_scam_age_group(dataset):
